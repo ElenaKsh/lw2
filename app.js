bf08d8b5d2e3a610955a6e433f86ded1da202783
Elena <olimpiada100@yandex.ru>
2020-10-21 13:32:34 +0300

    first part

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var authUserData = null;
 var userDatabase = [];
 function register(email, password) {
   let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(email) == false) {
+  if (re.test(email) === false) {
     return "Данный email невалиден";
   }
   let digits = "0123456789";
@@ -13,7 +13,11 @@ function register(email, password) {
     }
   }
 
-  if (password.length >= 6 && is_digit && (password.charAt(0).toUpperCase() == password.charAt(0))) {
+  if (
+    password.length >= 6 &&
+    is_digit &&
+    password.charAt(0).toUpperCase() === password.charAt(0)
+  ) {
     userData = [email, password];
     userDatabase.push(userData);
     return "Пользователь успешно добавлен";
@@ -23,26 +27,94 @@ function register(email, password) {
 }
 
 function signIn(email, password) {
-  // ваш код
-  // проверка на валидность email, пароля (6 символов, начинается с большой буквы)
-  // проверка наличия пользователя в userDatabase
-  // заполнение authUserData
+  let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(email) === false) {
+    return "Данный email невалиден";
+  }
+  let digits = "0123456789";
+  let is_digit = false;
+  for (let i = 0; i < password.length; i++) {
+    if (digits.indexOf(password[i]) != -1) {
+      is_digit = true;
+    }
+  }
+
+  if (
+    password.length >= 6 &&
+    is_digit &&
+    password.charAt(0).toUpperCase() === password.charAt(0)
+  ) {
+    for (let i = 0; i < userDatabase.length; i++) {
+      if (userDatabase[i][0] === email && userDatabase[i][1] === password) {
+        authUserData = true;
+        return "Вход выполнен";
+      }
+    }
+  } else {
+    return "Данный пароль не валиден";
+  }
 }
 
 function signOut() {
-  // ваш код
+  authUserData = null;
+  return "Выход выполнен";
 }
 
 function resetPassword(email, oldPassword, newPassword) {
-  // функция восстановления пароля
-  // должна изменять пароль пользователя если старый пароль введен верно и новый пароль соответствует правилам формата пароля
+  let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(email) === false) {
+    return "Данный email невалиден";
+  }
+  let digits = "0123456789";
+  let isDigitForOldPassword = false;
+  for (let i = 0; i < oldPassword.length; i++) {
+    if (digits.indexOf(oldPassword[i]) != -1) {
+      isDigitForOldPassword = true;
+    }
+  }
+
+  if (
+    oldPassword.length < 6 ||
+    !isDigitForOldPassword ||
+    oldPassword.charAt(0).toUpperCase() !== oldPassword.charAt(0)
+  ) {
+    return "Данный пароль не валиден";
+  }
+
+  let isDigitForNewPassword = false;
+  for (let i = 0; i < newPassword.length; i++) {
+    if (digits.indexOf(newPassword[i]) != -1) {
+      isDigitForNewPassword = true;
+    }
+  }
+
+  if (
+    newPassword.length < 6 ||
+    !isDigitForNewPassword ||
+    newPassword.charAt(0).toUpperCase() !== newPassword.charAt(0)
+  ) {
+    return "Данный пароль не валиден";
+  }
+
+  for (let i = 0; i < userDatabase.length; i++) {
+    if (userDatabase[i][0] === email && userDatabase[i][1] === oldPassword) {
+      userDatabase[i][1] = newPassword;
+      return "Парорль изменён";
+    }
+  }
 }
 
 function isAuth() {
-  // функция возвращает true если пользователь авторизован, false если нет
+  return authUserData;
 }
 
 //console.log(validator("25"));
 
-console.log(register('smyshlyaev12@list.ru','Password123'));
+console.log(register("smyshlyaev12@list.ru", "Password123"));
+console.log(signIn("smyshlyaev12@list.ru", "Password123"));
+console.log(
+  resetPassword("smyshlyaev12@list.ru", "Password123", "Password1478524758")
+);
+console.log(signOut());
 console.log(userDatabase);
+console.log(authUserData);
